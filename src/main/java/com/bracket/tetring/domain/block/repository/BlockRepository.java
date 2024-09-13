1bfd12f202b971601883939439d2de79f497f342
songhj <427shg@gmail.com>
2024-09-13 18:09:30 +0900

    fix: blockRepository 수정 StoreBlock에 없는 블록을 가져와야 함

--- src/main/java/com/bracket/tetring/domain/block/repository/BlockRepository.java
+++ src/main/java/com/bracket/tetring/domain/block/repository/BlockRepository.java
@@ -3,9 +3,13 @@ package com.bracket.tetring.domain.block.repository;
 import com.bracket.tetring.domain.block.domain.Block;
 import com.bracket.tetring.domain.game.domain.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface BlockRepository extends JpaRepository<Block, Long> {
-    List<Block> findByGame(Game game);
+
+    @Query("SELECT b FROM Block b WHERE b.game = :game AND TYPE(b) <> StoreBlock")
+    List<Block> findBlocksNotInStore(@Param("game") Game game);
 }

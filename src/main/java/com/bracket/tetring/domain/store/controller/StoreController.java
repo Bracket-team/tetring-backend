00521a8868f05c52e74b64f2815d57b01c510176
songhj <427shg@gmail.com>
2024-09-13 23:42:23 +0900

    feat: getStoreInfo 컨트롤러

---
+++ src/main/java/com/bracket/tetring/domain/store/controller/StoreController.java
@@ -0,0 +1,21 @@
+package com.bracket.tetring.domain.store.controller;
+
+import com.bracket.tetring.domain.store.service.StoreService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/stores")
+public class StoreController {
+    private final StoreService storeService;
+
+    @GetMapping("")
+    public ResponseEntity<?> getStoreInfo() {
+        return storeService.getStoreDetails();
+    }
+
+}

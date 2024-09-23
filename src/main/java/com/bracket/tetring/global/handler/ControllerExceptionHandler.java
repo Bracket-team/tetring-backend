167b351cd863732f752d136c1101cc622f0c55bc
songhj <427shg@gmail.com>
2024-09-23 22:47:33 +0900

    fix: 에러 처리 수정

--- src/main/java/com/bracket/tetring/global/handler/ControllerExceptionHandler.java
+++ src/main/java/com/bracket/tetring/global/handler/ControllerExceptionHandler.java
@@ -2,7 +2,6 @@ package com.bracket.tetring.global.handler;
 
 import com.bracket.tetring.global.dto.reponse.FailResponseDto;
 import com.bracket.tetring.global.error.ErrorCode;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,13 +13,6 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<FailResponseDto> customException(CustomException e) {
         return toResponse(e.getErrorCode(), e.getMessage());
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<FailResponseDto> handleAllExceptions(Exception e) {
-        // 다른 모든 예외 처리
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new FailResponseDto(ErrorCode.INTERNAL_SERVER_ERROR, e.getMessage()));
     }
 
     private static ResponseEntity<FailResponseDto> toResponse(ErrorCode errorCode, String message) {

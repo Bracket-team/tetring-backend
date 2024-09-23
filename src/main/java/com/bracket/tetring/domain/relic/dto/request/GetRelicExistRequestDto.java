b90538dc65fdade3183d26fd3108188e5c969bac
songhj <427shg@gmail.com>
2024-09-23 21:26:40 +0900

    fix: 역직렬화 문제 해결

--- src/main/java/com/bracket/tetring/domain/relic/dto/request/GetRelicExistRequestDto.java
+++ src/main/java/com/bracket/tetring/domain/relic/dto/request/GetRelicExistRequestDto.java
@@ -3,8 +3,10 @@ package com.bracket.tetring.domain.relic.dto.request;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class GetRelicExistRequestDto {
     @JsonProperty("relic_number")

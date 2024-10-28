9c68fe77c2cbbeca5fb24b458f4e735459b594e3
songhj <427shg@gmail.com>
2024-10-29 02:12:12 +0900

    fix : import 문 제거

--- src/main/java/com/bracket/tetring/global/handler/ControllerExceptionHandler.java
+++ src/main/java/com/bracket/tetring/global/handler/ControllerExceptionHandler.java
@@ -3,16 +3,20 @@ package com.bracket.tetring.global.handler;
 import com.bracket.tetring.global.dto.reponse.FailResponseDto;
 import com.bracket.tetring.global.error.ErrorCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@RestController
-@ControllerAdvice
-public class ControllerExceptionHandler {
+@RestControllerAdvice
+public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<FailResponseDto> customException(CustomException e) {
         return toResponse(e.getErrorCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<FailResponseDto> exception(Exception e) {
+        return toResponse(ErrorCode.INTERNAL_SERVER_ERROR, e.getMessage());
     }
 
     private static ResponseEntity<FailResponseDto> toResponse(ErrorCode errorCode, String message) {

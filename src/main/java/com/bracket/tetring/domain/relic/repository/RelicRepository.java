83193f590fdfe68f3aa2c0a1adecc94afce294b4
songhj <427shg@gmail.com>
2024-10-30 10:50:36 +0900

    gitignore 수정

--- src/main/java/com/bracket/tetring/domain/relic/repository/RelicRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bracket.tetring.domain.relic.repository;
-
-import com.bracket.tetring.domain.relic.domain.Relic;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RelicRepository extends JpaRepository<Relic, Integer> {
-}
--- src/main/java/com/bracket/tetring/global/oauth2/user/OAuth2UserUnlink.java
+++
@@ -1,5 +0,0 @@
-package com.bracket.tetring.global.oauth2.user;
-
-public interface OAuth2UserUnlink {
-    void unlink(String accessToken);
-}

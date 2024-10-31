b4f0910e0a1a7e13f409c9c624088a5945185a65
songhj <427shg@gmail.com>
2024-10-31 11:31:56 +0900

    feat: 목표 점수 수정, 클론 유물 제거

--- src/main/java/com/bracket/tetring/global/util/GameSettings.java
+++ src/main/java/com/bracket/tetring/global/util/GameSettings.java
@@ -6,7 +6,7 @@ public class GameSettings {
     public static final int BLOCK_PRICE = 2;
     public static final int REROLL_INITIAL_PRICE = 2;
     public static final int REROLL_UPDATE_PRICE = 1;
-    public static final int[] ROUND_GOALS = {800, 1400, 2000, 2400, 2800, 3200, 4000, 4600, 5000, 5800, 6200, 6600};
+    public static final int[] ROUND_GOALS = {600, 1400, 2800, 3600, 7200, 12000, 22000, 54000, 180000, 340000, 720000, 1508200};
     public static final int[] MONEY_LEVEL_UP_PRICE = {3, 5, 10, 20, 0};
     public static final int[] MONEY_GET = {1, 3, 5, 10, 20};
 
